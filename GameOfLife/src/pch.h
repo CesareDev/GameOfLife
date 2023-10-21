abc82dd4741476936be91f300176cef18f902536
CesareDev <corsicesare.lavoro@gmail.com>
2023-10-21 23:56:30 +0200

    Change window and cell size

--- GameOfLife/src/pch.h
+++ GameOfLife/src/pch.h
@@ -4,8 +4,8 @@
 #include <vector>
 #include <time.h>
 
-constexpr auto CELL_SIZE = 1;
-constexpr auto SCREEN_W = 128;
-constexpr auto SCREEN_H = 128;
+constexpr auto CELL_SIZE = 4;
+constexpr auto SCREEN_W = 1280;
+constexpr auto SCREEN_H = 720;
 constexpr auto ROWS = SCREEN_H / CELL_SIZE;
 constexpr auto COLS = SCREEN_W / CELL_SIZE;
